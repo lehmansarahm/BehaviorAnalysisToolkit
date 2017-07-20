a4a36a68398e41ee035d0dd9b2a02cc2e402c06a
Sarah Lehman <tug46038@temple.edu>
2017-07-20 16:25:27 -0400

    Now compiling as single EXE with embedded DLLs

---
+++ BAT.Core/Common/LogManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BAT.Core.Common
+{
+    public class LogManager
+    {
+        private static readonly log4net.ILog log = 
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Info the specified message.
+        /// </summary>
+        /// <returns>The info.</returns>
+        /// <param name="message">Message.</param>
+        public static void Info(string message) 
+        {
+            log.Info(message);
+        }
+
+        /// <summary>
+        /// Debug the specified message.
+        /// </summary>
+        /// <returns>The debug.</returns>
+        /// <param name="message">Message.</param>
+        public static void Debug(string message) 
+        {
+            log.Debug(message);
+        }
+
+        /// <summary>
+        /// Error the specified message.
+        /// </summary>
+        /// <returns>The error.</returns>
+        /// <param name="message">Message.</param>
+        public static void Error(string message) 
+        {
+            log.Error(message);
+        }
+    }
+}
--- BAT.MCI/Program.cs
+++ BAT.MCI/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using BAT.Core.Common;
+using BAT.Core.Config;
 
-namespace BehaviorAnalysisToolkit
+namespace BAT.MCI
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string configFilepath = "configuration.json";
+            if (args.Length >= 1) configFilepath = args[0]; 
+            else LogManager.Info("No configuration file provided.  Using default.");
+
+            try
+			{
+				var config = Configuration.LoadFromFile(configFilepath);
+				LogManager.Info("Configuration file loaded.");
+            } catch (Exception e) {
+                LogManager.Error("ERROR:  Could not load configuration file.  Exiting program.");
+            }
         }
     }
 }
